7db2fcd89b3354b020c1f05b5bcd2526f2b2e3d0
Safi <nassarsafi@gmail.com>
2019-11-14 19:36:58 +0200

    ✒ Change traversalInterception implementation to use a proxy function with only a single argument, and extract all other arguments from the target function call.

--- source/traversalImplementation/traversalInterception.js
+++ source/traversalImplementation/traversalInterception.js
@@ -2,10 +2,10 @@ import assert from 'assert'
 
 // visiting each node before visiting it's child nodes.
 // The middlewares that follow the Koa specification use next to call one another. In this case the nextFunction will be used instead, in which it controlls the propagation of nested traversal nodes.
-export const handleMiddlewareNextCall = ({ dataProcessCallback, targetFunction, aggregator }) => {
-  return new Proxy(targetFunction, {
+export const handleMiddlewareNextCall = targetFunction =>
+  new Proxy(targetFunction, {
     async apply(target, thisArg, argArray) {
-      let { nodeInstance, traversalDepth, eventEmitter } = argArray[0]
+      let { nodeInstance, traversalDepth, eventEmitter, processDataCallback, aggregator } = argArray[0]
       let nextCalled = false
       // A next function that will be used to compose in a sense the middlewares that are being executed during traversal. As middlewares relies on `next` function to chain the events.
       const nextFunction = async () => {
@@ -14,24 +14,23 @@ export const handleMiddlewareNextCall = ({ dataProcessCallback, targetFunction, 
         for await (let traversalResult of traversalResultIterator) aggregator.merge(traversalResult.result)
       }
 
-      let result = await dataProcessCallback({ nextProcessData: aggregator.value, additionalParameter: { nextFunction } })
+      let result = await processDataCallback({ nextProcessData: aggregator.value, additionalParameter: { nextFunction } })
       if (!nextCalled) await nextFunction() // in some cases the data process returns without calling nextFunction (when it is a regular node, not a process intending to execute a middleware).
 
       return traversalDepth == 0 ? aggregator.value : aggregator // check if top level call and not an initiated nested recursive call.
     },
   })
-}
 
 // visiting each node before visiting it's child nodes.
-export const processThenTraverse = ({ dataProcessCallback, targetFunction, aggregator }) => {
-  return new Proxy(targetFunction, {
+export const processThenTraverse = targetFunction =>
+  new Proxy(targetFunction, {
     async apply(target, thisArg, argArray) {
-      let { nodeInstance, traversalDepth, eventEmitter } = argArray[0]
+      let { nodeInstance, traversalDepth, eventEmitter, processDataCallback, aggregator } = argArray[0]
       eventEmitter.on('nodeTraversalCompleted', data => {
         // console.log(data.value, ' resolved.')
       })
 
-      let result = await dataProcessCallback({ nextProcessData: aggregator.value, additionalParameter: {} })
+      let result = await processDataCallback({ nextProcessData: aggregator.value, additionalParameter: {} })
 
       let traversalResultIterator = await Reflect.apply(...arguments)
       for await (let traversalResult of traversalResultIterator) aggregator.merge(traversalResult.result)
@@ -39,13 +38,12 @@ export const processThenTraverse = ({ dataProcessCallback, targetFunction, aggre
       return traversalDepth == 0 ? aggregator.value : aggregator // check if top level call and not an initiated nested recursive call.
     },
   })
-}
 
 // vising the node after visiting the child nodes.
-export const traverseThenProcess = ({ dataProcessCallback, targetFunction, aggregator }) => {
-  return new Proxy(targetFunction, {
+export const traverseThenProcess = targetFunction =>
+  new Proxy(targetFunction, {
     async apply(target, thisArg, argArray) {
-      let { nodeInstance, traversalDepth, eventEmitter } = argArray[0]
+      let { nodeInstance, traversalDepth, eventEmitter, processDataCallback, aggregator } = argArray[0]
       eventEmitter.on('nodeTraversalCompleted', data => {
         // console.log(data.value, ' resolved.')
       })
@@ -53,18 +51,17 @@ export const traverseThenProcess = ({ dataProcessCallback, targetFunction, aggre
       let traversalResultIterator = await Reflect.apply(...arguments)
       for await (let traversalResult of traversalResultIterator) aggregator.merge(traversalResult.result)
 
-      let result = await dataProcessCallback({ nextProcessData: aggregator.value, additionalParameter: {} })
+      let result = await processDataCallback({ nextProcessData: aggregator.value, additionalParameter: {} })
 
       return traversalDepth == 0 ? aggregator.value : aggregator // check if top level call and not an initiated nested recursive call.
     },
   })
-}
 
 // returns the process result of the root node, while returnning the aggregator for any nested nodes that will eventually be merged together through the Aggregator implementation. Used for CONFIGURE relationship with case switches.
-export const traverseThenProcessWithLogicalOperator = ({ dataProcessCallback, targetFunction, aggregator }) => {
-  return new Proxy(targetFunction, {
+export const traverseThenProcessWithLogicalOperator = targetFunction =>
+  new Proxy(targetFunction, {
     async apply(target, thisArg, argArray) {
-      let { nodeInstance, traversalDepth, eventEmitter } = argArray[0]
+      let { nodeInstance, traversalDepth, eventEmitter, processDataCallback, aggregator } = argArray[0]
       eventEmitter.on('nodeTraversalCompleted', data => {
         // console.log(data.value, ' resolved.')
       })
@@ -78,24 +75,8 @@ export const traverseThenProcessWithLogicalOperator = ({ dataProcessCallback, ta
         aggregator.merge(traversalResult.result, undefined, logicalOperator)
       }
 
-      let result = await dataProcessCallback({ nextProcessData: aggregator.calculatedLogicalOperaion, additionalParameter: {} })
+      let result = await processDataCallback({ nextProcessData: aggregator.calculatedLogicalOperaion, additionalParameter: {} })
 
       return traversalDepth == 0 ? [result] : aggregator // check if top level call and not an initiated nested recursive call.
     },
   })
-}
-
-// export const traverseThenProcessForSwitch = ({ dataProcessCallback, targetFunction, aggregator }) => {
-//   return new Proxy(targetFunction, {
-//     async apply(target, thisArg, argArray) {
-//       let { nodeInstance, traversalDepth, eventEmitter } = argArray[0]
-
-//       let traversalResultIterator = await Reflect.apply(...arguments)
-//       for await (let traversalResult of traversalResultIterator) aggregator.merge(traversalResult.result)
-
-//       let result = await dataProcessCallback({ nextProcessData: aggregator.value, additionalParameter: {} })
-
-//       return traversalDepth == 0 ? aggregator.value : aggregator // check if top level call and not an initiated nested recursive call.
-//     },
-//   })
-// }
