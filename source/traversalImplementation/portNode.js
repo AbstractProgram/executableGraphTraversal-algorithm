abe5991630d8521def54355ed6a38eb38dafe37f
Safi <nassarsafi@gmail.com>
2020-01-13 00:26:53 +0200

    ⚗ Update rest of parameters affected by refactoring.

--- source/traversalImplementation/portNode.js
+++ source/traversalImplementation/portNode.js
@@ -4,8 +4,8 @@ import { resolve } from 'path'
 /**  The purpose of this function is to find & yield next nodes.
  * @yields {Object { node: <node instance>} } a traversal configuration feed/iterato
  **/
-export async function* nestedNode({ forkEdge, getImplementation, additionalChildNode, graph }) {
+export async function* nestedNode({ forkEdge, getImplementation, additionalChildNode, traverser = this }) {
   let portNode = forkEdge.destination
-  let nodeIteratorFeed = await graph.traverserInstruction.nodeIterator.iterateNext({ targetNode: forkEdge.destination, additionalChildNode, graph })
+  let nodeIteratorFeed = await traverser::traverser.traverserInstruction.nodeIterator.iterateNext({ targetNode: portNode, additionalChildNode })
   yield* nodeIteratorFeed
 }
